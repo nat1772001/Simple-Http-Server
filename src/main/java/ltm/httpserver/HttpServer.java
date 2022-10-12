754e14a0a3de96be34011778d4b345ec4d49554c
nat1772001 <90968896+nat1772001@users.noreply.github.com>
2022-10-13 03:07:52 +0700

    using thread for multiple connections and using slf4j for Logging

--- src/main/java/ltm/httpserver/HttpServer.java
+++ src/main/java/ltm/httpserver/HttpServer.java
@@ -2,6 +2,10 @@ package ltm.httpserver;
 
 import ltm.httpserver.config.Configuration;
 import ltm.httpserver.config.ConfigurationManager;
+import ltm.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,42 +14,22 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
     public static void main(String[] args) {
 
-        System.out.println("Server starting...");
+        LOGGER.info("Server starting...");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("Using Port:" + conf.getPort());
-        System.out.println("Using WebRoot:" + conf.getWebRoot());
+        LOGGER.info("Using Port:" + conf.getPort());
+        LOGGER.info("Using WebRoot:" + conf.getWebRoot());
 
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
-
-            final String CRLF = "\n\r";
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF +
-                    "Content-Length: " +html.getBytes().length + CRLF +
-                    CRLF +
-                    html +
-                    CRLF + CRLF;
-
-            outputStream.write((response.getBytes()));
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebRoot());
+            serverListenerThread.start();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
     }
