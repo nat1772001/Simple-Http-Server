18eaf75791a6fb3f9ba0b08a78abb72ba471f430
nat1772001 <90968896+nat1772001@users.noreply.github.com>
2022-10-13 02:32:37 +0700

    simple request/response with ServerSocket

--- src/main/java/ltm/httpserver/HttpServer.java
+++ src/main/java/ltm/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package ltm.httpserver;
 import ltm.httpserver.config.Configuration;
 import ltm.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
     public static void main(String[] args) {
 
@@ -13,5 +19,34 @@ public class HttpServer {
 
         System.out.println("Using Port:" + conf.getPort());
         System.out.println("Using WebRoot:" + conf.getWebRoot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r";
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +
+                    "Content-Length: " +html.getBytes().length + CRLF +
+                    CRLF +
+                    html +
+                    CRLF + CRLF;
+
+            outputStream.write((response.getBytes()));
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
